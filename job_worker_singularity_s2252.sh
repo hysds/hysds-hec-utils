56c6104a50c0c251a344dc854647767c75d6a8db
Lei Pan <leipan@jpl.nasa.gov>
2020-07-23 15:29:20 -0700

    added group id s2252

---
+++ job_worker_singularity_s2252.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# pbs init stuff
+export PBS_JOBID=$1
+WORKER_ID="pleiades_worker.${PBS_JOBID}"
+TOKENS=$(date +"%Y %m %d")
+IFS=" " read YEAR MONTH DAY <<< ${TOKENS}
+TIMESTAMP=$(date +%Y%m%dT%H%M%S)
+export SANDBOX_DIR="/nobackupp12/lpan/PGE/container-aria-jpl_ariamh_develop_singularity-2020-07-10-1bbb06d33ce6.simg"
+### export HYSDS_CELERY_CFG="/home1/lpan/verdi/ops/hysds/e_celeryconfig.py"
+### export HYSDS_CELERY_CFG_MODULE="e_celeryconfig"
+export HYSDS_CELERY_CFG="/home1/lpan/verdi/ops/hysds/mamba_celeryconfig.py"
+export HYSDS_CELERY_CFG_MODULE="mamba_celeryconfig"
+export NOBACKUP="/nobackupp12/lpan/s2252"
+### export HYSDS_DATASETS_CFG="/home1/lpan/verdi/etc/e_cluster_config/datasets.json"
+export HYSDS_DATASETS_CFG="/home1/lpan/verdi/etc/mamba_config/datasets.json"
+#
+### export HYSDS_ROOT_CACHE_DIR="${TMPDIR}/"
+### echo "TMPDIR=${TMPDIR}"
+### echo $(df -h ${TMPDIR})
+#
+# the top-level worker directory for each job worker
+WORKER_DIR="${NOBACKUP}/worker/$YEAR/$MONTH/$DAY/$TIMESTAMP-$WORKER_ID"
+LOG_DIR="${NOBACKUP}/worker/logs/$YEAR/$MONTH/$DAY/$TIMESTAMP-$WORKER_ID"
+export HYSDS_ROOT_WORK_DIR="${WORKER_DIR}/work/"
+mkdir -p ${HYSDS_ROOT_WORK_DIR}
+export HYSDS_ROOT_CACHE_DIR="${WORKER_DIR}/cache/"
+mkdir -p ${HYSDS_ROOT_CACHE_DIR}
+mkdir -p ${LOG_DIR}
+LOGFILE="${LOG_DIR}/$TIMESTAMP-$WORKER_ID.log"
+cd $HYSDS_ROOT_WORK_DIR
+# ensure that job gets all 28 Broadwell cores
+export OMP_NUM_THREADS=28
+env > $LOGFILE
+# run celery job worker & # in background, but save its PID
+celery worker --app=hysds --concurrency=1 --loglevel=INFO -Q standard_product-s1gunw-topsapp-pleiades_s2252 -n $WORKER_ID -O fair --without-mingle --without-gossip --heartbeat-interval=60 >> $LOGFILE 2>&1 &
+#
+CELERY_JOB_WORKER_PID=$!
+
+function cleanup() {
+  echo "Caught SIGTERM signal. Killing $CELERY_JOB_WORKER_PID ..."
+  if kill -0 $CELERY_JOB_WORKER_PID
+  then
+    kill -TERM "$CELERY_JOB_WORKER_PID"
+  fi
+
+  ### echo "TMPDIR=${TMPDIR}"
+  ### echo $(df -h ${TMPDIR})
+
+  # clean up after the pbs job is done
+  echo "removing WORKER_DIR: $WORKER_DIR"
+  rm -rf "$WORKER_DIR"
+}
+trap cleanup SIGTERM
+trap cleanup EXIT
+
+echo "WORKER_DIR: $WORKER_DIR"
+echo "CELERY_JOB_WORKER_PID: $CELERY_JOB_WORKER_PID"
+python /home1/lpan/verdi/ops/hysds/scripts/harikiri_pid.py  $HYSDS_ROOT_WORK_DIR -i 300 -c 120 -p $CELERY_JOB_WORKER_PID -l https://tpfe2.nas.nasa.gov:8443/mozart/api/v0.1/
+
+
