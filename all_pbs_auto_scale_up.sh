a305d0d934c0680cc3bfe617468bbfe9aa354a07
Lei Pan <leipan@jpl.nasa.gov>
2020-11-19 15:37:54 -0800

    use input arg for MAX_WORKERS

--- all_pbs_auto_scale_up.sh
+++ all_pbs_auto_scale_up.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-nohup ./pbs_auto_scale_up.sh s2037 140 > pbs_auto_scale_up-s2037.log 2>&1 &
-nohup ./pbs_auto_scale_up.sh s2252 140 > pbs_auto_scale_up-s2252.log 2>&1 &
-nohup ./pbs_auto_scale_up.sh s2310 140 > pbs_auto_scale_up-s2310.log 2>&1 &
+MAX_WORKERS=$1
+
+nohup ./pbs_auto_scale_up.sh s2037 $MAX_WORKERS > pbs_auto_scale_up-s2037.log 2>&1 &
+nohup ./pbs_auto_scale_up.sh s2252 $MAX_WORKERS > pbs_auto_scale_up-s2252.log 2>&1 &
+nohup ./pbs_auto_scale_up.sh s2310 $MAX_WORKERS > pbs_auto_scale_up-s2310.log 2>&1 &
