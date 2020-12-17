4983882ea3d64cfe1489ec79d0ac5c4438898d8c
Lei Pan <leipan@jpl.nasa.gov>
2020-12-17 04:48:18 -0800

    updated mtime tighter

--- purge_old_files.sh
+++ purge_old_files.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-find /nobackupp12/esi_sar/s2037/worker/ -type f -mtime +2.6 | xargs rm -f
+find /nobackupp12/esi_sar/s2037/worker/ -type f -mtime +2.1 | xargs rm -f
 find /nobackupp12/esi_sar/s2037/worker/ -type d -empty -delete
-find /nobackupp12/esi_sar/s2252/worker/ -type f -mtime +2.6 | xargs rm -f
+find /nobackupp12/esi_sar/s2252/worker/ -type f -mtime +2.1 | xargs rm -f
 find /nobackupp12/esi_sar/s2252/worker/ -type d -empty -delete
-find /nobackupp12/esi_sar/s2310/worker/ -type f -mtime +2.6 | xargs rm -f
+find /nobackupp12/esi_sar/s2310/worker/ -type f -mtime +2.1 | xargs rm -f
 find /nobackupp12/esi_sar/s2310/worker/ -type d -empty -delete
 
 # make these purging command a cron job by running: crontab /home4/esi_sar/github/hysds-hec-utils/crontab_purge.txt
